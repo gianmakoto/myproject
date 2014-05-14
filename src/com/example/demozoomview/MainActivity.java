3b7c47ff016c298e757f8268bcfdeb0d5b292b0c
Giancarlo <giannmakoto@gmail.com>
2014-05-14 14:42:35 -0500

    df

--- src/com/example/demozoomview/MainActivity.java
+++ src/com/example/demozoomview/MainActivity.java
@@ -24,16 +24,12 @@ public class MainActivity extends FragmentActivity {
 	private Animator mCurrentAnimator;
 	private int mShortAnimationDuration;
 	
-
 	private int imagenes[] = { R.drawable.descarga, R.drawable.pikachu,
 			R.drawable.logoaudition, R.drawable.audition07, 
 			R.drawable.descarga1, R.drawable.descarga2, R.drawable.descarga3, R.drawable.descarga4,
 		    R.drawable.descarga5, R.drawable.dota1, R.drawable.dota2, R.drawable.dota3, R.drawable.dota4 };
 
 	
-
-	//nose
-	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
